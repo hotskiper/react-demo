9c79ccf7b92ef35759ba72e36519052602e73a9e
hotskiper <380311869@qq.com>
2021-01-07 19:47:45 +0800

    update

--- src/containers/popular/index.js
+++ src/containers/popular/index.js
@@ -1,9 +1,64 @@
+import React, {useState, useEffect, useContext} from 'react'
+
+const TestContext = React.createContext()
 function App(props) {
+  // const[top, setTop] = useState('top');
+  const testVal = 'aaa'
   return (
-    <div className="App">
-      Popular
+    <TestContext.Provider value={{uname: testVal}}>
+      <div className="App">
+        Popular
+        <StateComp />
+        <Counter />
+      </div>
+    </TestContext.Provider>
+  )
+}
+
+function StateComp(props){
+  const[name, setName] = useState('init');
+  const[obj, setObj] = useState(null);
+  const { uname } = useContext(TestContext)
+  const handleClick = () => { 
+    setName(prev => prev + 'asd');
+    console.log(uname)
+    
+    setObj({...obj, a: 2})
+  }
+  return (
+    <div onClick={()=>{
+      handleClick();
+    }}>{name}{obj?obj.a:''}
+    <p></p>
     </div>
   )
 }
+
+function Counter(props){
+  const [count, setCount] = useState(0);
+  const [obj, setObj] = useState({num: 0})
+  const { uname } = useContext(TestContext)
+  useEffect(() => {
+    document.title = count;
+    return () => {
+      document.title = uname
+    }
+  })
+  const handleClick = ()=>{
+    
+    for(let i = 0 ; i < 3 ; i++){
+      console.log(666);
+      // setCount(count + 1)
+      setCount(prev => prev + 1)
+      let newObj = obj;
+      newObj.num++;
+      setObj(newObj)
+    }
+  }
+  return (
+    <div onClick={handleClick}>{count},{obj.num}</div>
+  )
+}
+
 
 export default App
